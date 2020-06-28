37ae30aafd07a8789ab5d366e2392e525c8634ff
jingwen kuang <jwkwendy@gmail.com>
2020-06-28 00:45:31 -0700

    update pin index item

--- frontend/components/pins/pin_index_item.jsx
+++ frontend/components/pins/pin_index_item.jsx
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 const PinIndexItem = ({ pin }) => {
   
   return (
-    <div className='pin-link'>
-      <Link to={`pin/${pin.id}`}>
-      <img src={pin.photoUrl} className='pin-index'/>
-      </Link>
-    </div>
+    <div className='pin-index-item'>
+      <div className='pin-link'>
+        <Link to={`pin/${pin.id}`}>
+        <img src={pin.photoUrl} className='pin-index'/>
+       </Link>
+      </div>
 
-    <div>
-      <div className='pin-title'>{pin.title}</div>
+      <div>
+        <div className='pin-title'>{pin.title}</div>
+      </div>
+
     </div>
   )
 }
