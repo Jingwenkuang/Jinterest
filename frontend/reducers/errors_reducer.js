e35b6073c577efd38a0bb53222096c395d5576f1
jingwen kuang <jwkwendy@gmail.com>
2020-08-05 13:28:33 -0700

    Update errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import pinErrorsReducer from './pin_errors_reducer';
-// import userErrorsReducer from './user_errors_reducer';
+import userErrorsReducer from './user_errors_reducer';
+import boardsErrorsReducer from './boards_errors_reducer';
+import boardsPinsErrorsReducer from './boards_pins_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   pins: pinErrorsReducer,
-  // users: userErrorsReducer, 
+  users: userErrorsReducer, 
+  boards: boardsErrorsReducer,
+  boardsPins: boardsPinsErrorsReducer
 })
 
 export default errorsReducer;
