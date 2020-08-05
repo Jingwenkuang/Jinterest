1872deb6b9028af6adf55481e4b4935c50deecf9
jingwen kuang <jwkwendy@gmail.com>
2020-08-05 13:25:08 -0700

    Update entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import pinsReducer from './pins_reducer';
+import boardsReducer from './boards_reducer';
+import boardsPinsReducer from './board_pins_reducer';
+
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   pins: pinsReducer,
+  boards: boardsReducer,
+  boardsPins: boardsPinsReducer
 });
 
 export default entitiesReducer;
