5b43a2e0c3e2307a36a39927070a7b804bf3efb0
jingwen kuang <jwkwendy@gmail.com>
2020-08-05 13:19:14 -0700

    Add board reducer

---
+++ frontend/reducers/board_pins_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_ALL_BOARDS_PINS, RECEIVE_BOARD_PIN, REMOVE_BOARD_PIN } from "../actions/board_pin_actions";
+
+const boardsPinsReducer = (state={}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_ALL_BOARDS_PINS:
+      return action.boardsPins;
+    case RECEIVE_BOARD_PIN:
+      return Object.assign({}, state, { [action.boardPin.id]: action.boardPin });
+    case REMOVE_BOARD_PIN:
+      let newState = Object.assign({}, state);
+      delete newState[action.boardPinId];
+      return newState;
+    default: 
+      return state;
+  }
+}
+
+export default boardsPinsReducer;
---
+++ frontend/reducers/boards_errors_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_BOARD_ERRORS } from "../actions/board_actions";
+import { CLEAR_ERRORS } from "../actions/session_actions";
+
+const boardsErrorsReducer = (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_BOARD_ERRORS:
+      return action.errors;
+    case CLEAR_ERRORS:
+      return [];
+    default: 
+      return state;
+  }
+}
+
+export default boardsErrorsReducer;
