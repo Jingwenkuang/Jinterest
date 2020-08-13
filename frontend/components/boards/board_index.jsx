b5e9ac3da85bee47f1be8e3025d125e81ea5f0d9
jingwen kuang <jwkwendy@gmail.com>
2020-08-12 18:36:37 -0700

    Add board index container

--- frontend/components/boards/board_index.jsx
+++ frontend/components/boards/board_index.jsx
@@ -1,83 +1,136 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import BoardIndexItem from './board_index_item';
+
+
+// class BoardIndex extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.openEditBoard = this.openEditBoard.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.props.fetchBoards();
+//     this.props.fetchAllPins();
+//     this.props.fetchAllBoardsPins();
+//     this.props.fetchUser(this.props.userId)
+//   }
+
+  
+//   openEditBoard(e, boardId) {
+//     e.preventDefault();
+//     this.props.openEditBoard(boardId);
+//   }
+ 
+
+//   render() {
+//     const { boards, pins, currentUser, user, newBoard } = this.props;
+   
+//   console.log('check')
+//   console.log(pins)
+//   console.log(boards)
+  
+//     const boardIndexItems = (boards.length > 0) ? (
+//       boards.map(board => {
+//         const prevPinIds = (board.pinIds) ? (
+//           board.pinIds.slice(0, 6)
+//         ) : (
+//             board.pinIds
+//           );
+     
+//           const previewPins = prevPinIds.map((pinId) => {
+//             return pins[pinId];
+//           });
+       
+//         return (
+//           <BoardIndexItem
+//             key={board.id}
+//             board={board}
+//             pins={previewPins}
+//             currentUser={currentUser}
+//             user={user}
+//             openEditBoard={this.openEditBoard}
+//           />
+//         )
+//       })
+//     ) : (
+//         null 
+//       );
+
+//     return (
+//       <div className="board-index">
+//         {boardIndexItems}
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//         <div className="board-index-item filler"></div>
+//       </div>
+//     )
+//   }
+
+
+// }
+
+
+// export default BoardIndex;
+
+import React from 'react';
 import BoardIndexItem from './board_index_item';
 
-
-class BoardIndex extends Component {
+class BoardIndex extends React.Component {
   constructor(props) {
     super(props);
-    this.openEditBoard = this.openEditBoard.bind(this);
+    this.filterBoards = this.filterBoards.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchBoards();
     this.props.fetchAllPins();
-    this.props.fetchAllBoardsPins();
-    this.props.fetchUser(this.props.userId)
   }
 
-  
-  openEditBoard(e, boardId) {
-    e.preventDefault();
-    this.props.openEditBoard(boardId);
+  filterBoards() {
+
+    let userId = this.props.currentUser.id;
+
+    const { boards } = this.props;
+
+    return boards.filter(board => board.user_id === userId);
+
   }
- 
+
 
   render() {
-    const { boards, pins, currentUser, user, newBoard } = this.props;
-   
-  console.log('check')
-  console.log(pins)
-  console.log(boards)
-  
-    const boardIndexItems = (boards.length > 0) ? (
-      boards.map(board => {
-        const prevPinIds = (board.pinIds) ? (
-          board.pinIds.slice(0, 6)
-        ) : (
-            board.pinIds
-          );
-     
-          const previewPins = prevPinIds.map((pinId) => {
-            return pins[pinId];
-          });
-       
-        return (
-          <BoardIndexItem
-            key={board.id}
-            board={board}
-            pins={previewPins}
-            currentUser={currentUser}
-            user={user}
-            openEditBoard={this.openEditBoard}
-          />
-        )
-      })
-    ) : (
-        null 
-      );
+
+    if (!this.props.boards) {
+      return null;
+    }
+    let userBoards = this.filterBoards();
+    const boardIndexItems = userBoards.map(board => {
+      return <BoardIndexItem key={board.id} board={board} boardId={board.id} />
+    });
 
     return (
-      <div className="board-index">
-        {boardIndexItems}
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
-        <div className="board-index-item filler"></div>
+      <div className="boards-index">
+
+        <div className="sub-nav">
+          <button className="board-button">Boards</button>
+        </div>
+
+        <div className="boards-container">
+          <ul className="board-tab">
+            {boardIndexItems}
+          </ul>
+        </div>
       </div>
     )
   }
-
-
 }
 
-
 export default BoardIndex;
-
--- frontend/components/boards/board_index_item.jsx
+++ frontend/components/boards/board_index_item.jsx
@@ -1,68 +1,133 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
+// import React from "react";
+// import { Link, withRouter } from "react-router-dom";
 
 
-const BoardIndexItem = ({ board, pins, currentUser, user, openEditBoard }) => {
-  const numPins = board.pinIds.length;
+// const BoardIndexItem = ({ board, pins, currentUser, user, openEditBoard }) => {
+//   const numPins = board.pinIds.length;
 
-  const pinA = (pins[0]) ? <img src={`${pins[0].photo}`}></img> : null;
-  const pinB = (pins[1]) ? <img src={`${pins[1].photo}`}></img> : null;
-  const pinC = (pins[2]) ? <img src={`${pins[2].photo}`}></img> : null;
-  const pinD = (pins[3]) ? <img src={`${pins[3].photo}`}></img> : null;
-  const pinE = (pins[4]) ? <img src={`${pins[4].photo}`}></img> : null;
-  const pinF = (pins[5]) ? <img src={`${pins[5].photo}`}></img> : null;
+//   const pinA = (pins[0]) ? <img src={`${pins[0].photo}`}></img> : null;
+//   const pinB = (pins[1]) ? <img src={`${pins[1].photo}`}></img> : null;
+//   const pinC = (pins[2]) ? <img src={`${pins[2].photo}`}></img> : null;
+//   const pinD = (pins[3]) ? <img src={`${pins[3].photo}`}></img> : null;
+//   const pinE = (pins[4]) ? <img src={`${pins[4].photo}`}></img> : null;
+//   const pinF = (pins[5]) ? <img src={`${pins[5].photo}`}></img> : null;
 
  
-  const name = (currentUser.username === user.username) ? 'show' : 'hide';
+//   const name = (currentUser.username === user.username) ? 'show' : 'hide';
 
-  return (
-    <div className="board-index-item container">
-      <Link
-        to={{
-          pathname: `/${user.username}/${board.name}`,
-          state: {
-            fromProfile: true
-          }
-        }}
-        className="board-index-item link"
-      >
-        <div className="board-index-item content">
-          <div className="board-index-item hover-overlay"></div>
-          <div>
-            <div className="board-index-item pins-container">
-              <div className="board-index-item pins">
-                <div className="board-index-item pin" id="a">{pinA}</div>
-                <div className="board-index-item pin" id="b">{pinB}</div>
-                <div className="board-index-item pin" id="c">{pinC}</div>
-                <div className="board-index-item pin" id="d">{pinD}</div>
-                <div className="board-index-item pin" id="e">{pinE}</div>
-                <div className="board-index-item pin" id="f">{pinF}</div>
-              </div>
-            </div>
-            <div className="board-index-item info">
-              <div className="board-index-item info-details">
-                <div className="board-index-item title">
-                  {board.name}
-                </div>
+//   return (
+//     <div className="board-index-item container">
+//       <Link
+//         to={{
+//           pathname: `/${user.username}/${board.name}`,
+//           state: {
+//             fromProfile: true
+//           }
+//         }}
+//         className="board-index-item link"
+//       >
+//         <div className="board-index-item content">
+//           <div className="board-index-item hover-overlay"></div>
+//           <div>
+//             <div className="board-index-item pins-container">
+//               <div className="board-index-item pins">
+//                 <div className="board-index-item pin" id="a">{pinA}</div>
+//                 <div className="board-index-item pin" id="b">{pinB}</div>
+//                 <div className="board-index-item pin" id="c">{pinC}</div>
+//                 <div className="board-index-item pin" id="d">{pinD}</div>
+//                 <div className="board-index-item pin" id="e">{pinE}</div>
+//                 <div className="board-index-item pin" id="f">{pinF}</div>
+//               </div>
+//             </div>
+//             <div className="board-index-item info">
+//               <div className="board-index-item info-details">
+//                 <div className="board-index-item title">
+//                   {board.name}
+//                 </div>
           
-                  <div className="board-index-item pin-count">
-                    {numPins} Pins
-                  </div>
+//                   <div className="board-index-item pin-count">
+//                     {numPins} Pins
+//                   </div>
               
-              </div>
-              <button
-                className={`board-index-item edit-button ${name}`}
-                onClick={(e, boardId) => openEditBoard(e, board.id)}
-              >
-                <i className="fas fa-pencil-alt board-index-item" id="edit-icon"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
+//               </div>
+//               <button
+//                 className={`board-index-item edit-button ${name}`}
+//                 onClick={(e, boardId) => openEditBoard(e, board.id)}
+//               >
+//                 <i className="fas fa-pencil-alt board-index-item" id="edit-icon"></i>
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default withRouter(BoardIndexItem);
+
+import React from 'react';
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import BoardIndex from "./board_index";
+import { editBoard, deleteBoard } from "../../actions/board_actions";
+import { openModal, closeModal } from "../../actions/modal_actions";
+import EditBoardFormContainer from './board_edit_form_container';
+
+
+const msp = (state, ownProps) => ({
+  pins: state.entities.pins
+ 
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    editBoard: (id) => dispatch(openModal("editBoard", { id })),
+    deleteBoard: boardId => dispatch(deleteBoard(boardId)),
+  }
 };
 
-export default withRouter(BoardIndexItem);
 
+class BoardIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.redirect = this.redirect.bind(this);
+    this.filterPins = this.filterPins.bind(this)
+  }
+
+  redirect() {
+    this.props.history.push(`/boards/${this.props.board.id}`)
+  }
+
+  filterPins() {
+    let boardId = this.props.boardId;
+    let pins = Object.values(this.props.pins);
+
+    return pins.filter(pin => pin.board_id === boardId);
+
+  }
+
+  render() {
+    if (!this.props.pins) {
+      return null;
+    }
+    let title = this.props.board.title
+    let cover = this.filterPins()
+    let photo;
+    if (cover.length) photo = cover[0].photoUrl
+    return (
+      <>
+        <div className='board-index'>
+          <div className="board-container" onClick={this.redirect}>
+            <img className="board-cover" src={photo} />
+            <div className='board-box'>box</div>
+
+            <div className="board-title">{title}</div>
+            <div className="pins-amount">{cover.length} pins</div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+export default withRouter(connect(msp, mapDispatchToProps)(BoardIndexItem));
